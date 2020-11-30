8089bd9ef98a2741ec041b7ada35f9ade915a57e
benjaSanatana <0211518@up.edu.mx>
2020-11-30 20:09:40 +0000

    Saves files in file system

--- print_api.py
+++ print_api.py
@@ -6,25 +6,43 @@
 #
 # pylint: disable = missing-docstring 
 # pylint: disable = too-few-public-methods 
+import os
 import flask
-import flask_restful
+import werkzeug.utils
+
+UPLOAD_FOLDER = '/home/pi/print_api/uploaded_files'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'HEIC'}
 
 app = flask.Flask(__name__)
-api = flask_restful.Api(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-class Printy(flask_restful.Resource):
-    """
-    This class manage the request for printing things
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-    post -> recive a document and print it
-    """
-    def post(self):
-        return 0;
-
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return flask.render_template("index.html")
+    if flask.request.method == 'GET': 
+        return flask.render_template("index.html")
+
+    if flask.request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in flask.request.files:
+            flask.flash('No file part')
+            return flask.redirect(request.url)
+        file = flask.request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flask.flash('No selected file')
+            return flask.redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = werkzeug.utils.secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #os.system('lp -d HPLaserJ {0}')
+            return "done"
+        
+        return flask.render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port='8080')
